8d730c1901119e33d7cd2b27a737f92e3fd15f15
Xavier Garrido <xavier.garrido@gmail.com>
2014-04-16 16:47:50 +0200

    remove tex files

--- zsh-org-pages.zsh
+++ zsh-org-pages.zsh
@@ -181,7 +181,7 @@ function org-pages ()
 
     if [ ${keep_tmp_files} -eq 0 ]; then
         pkgtools__msg_debug "Remove useless files"
-        find . -regex ".*\.\(pyg\|auxlock\|toc\|out\|fls\|aux\|log\|fdb_latexmk\)" \
+        find . -regex ".*\.\(pyg\|auxlock\|toc\|out\|fls\|aux\|log\|fdb_latexmk\|tex\)" \
             -not -path '*doc*' -not -path '*figures*' -exec rm -f {} \;
         find . -name "*~" -exec rm -rf {} \;
         #find . -name "*latex.d*" -exec rm -rf {} \;
